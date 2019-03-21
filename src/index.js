ff91900b547d0ddb56248f0a3a9ecb80c6e2da36
ArturKuz <artkuznetsov191@gmail.com>
2019-03-21 15:46:56 -0700

    master/feat(brackets): add part of the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+  let stack = [];
+
+   for (var i = 0; i < str.length; i++) {
+    if (isParanthesis(str[i])) {
+      if (isOpenParenthesis(str[i])) {
+        stack.push(str[i]);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        var top = stack.pop(); // pop off the top element from stack
+        if (!matches(top, str[i])) {
+          return false;
+        }
+      }
+    }
+
+    function isParanthesis(char) {
+      let examp = '{}[]()||1234567788';
+      if (examp.indexOf(char) > -1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function isOpenParenthesis(parenthesisChar) {
+      for (var j = 0; j < bracketsConfig.length; j++) {
+        if (bracketsConfig[j][0] === parenthesisChar) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function matches(topOfStack, closedParenthesis) {
+      for (var k = 0; k < bracketsConfig.length; k++) {
+        if (bracketsConfig[k][0] === topOfStack && bracketsConfig[k][1] === closedParenthesis) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+  }
+  var result = stack.length === 0 ? true : false;
+  return result 
 }
